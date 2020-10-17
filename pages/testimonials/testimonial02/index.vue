43537754a403707e0e1b2e2594d1e5e10e17e2af
shane-moore <skm1790@gmail.com>
2020-10-17 16:16:07 -0500

    started the testimonials page

---
+++ pages/testimonials/testimonial02/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <section class="testimonial-container">
+    <article class="testimonial">
+      <p>
+        Working with Shane was better than expected and we had really high
+        expectations. He is an incredibly talented developer but what really
+        makes him stand out is his work ethic and steady approach. Time after
+        time, and without us asking, he added enhancements and improvements that
+        resulted in a better end product for us and our clients.
+      </p>
+    </article>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Testimonial02',
+}
+</script>
+
+<style scoped>
+.testimonial-container {
+  box-sizing: border-box;
+  width: 1000px;
+  padding: 8px;
+  border: 1px solid #ccc;
+  box-shadow: 0 2px 2px #eee;
+}
+</style>
