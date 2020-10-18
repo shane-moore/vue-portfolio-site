5236eaf743bcef4afa5a68ee2357608e41215d40
shane-moore <skm1790@gmail.com>
2020-10-18 08:10:28 -0500

    created footer

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -1,28 +1,28 @@
 <template>
   <div>
-    <b-navbar toggleable="lg" type="dark" variant="info">
-      <b-navbar-brand href="#">Shane Moore</b-navbar-brand>
-
-      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-
-      <b-collapse id="nav-collapse" is-nav>
-        <b-navbar-nav class="ml-auto">
-          <b-nav-item size="md"> Home </b-nav-item>
-          <b-nav-item> About </b-nav-item>
-          <b-nav-item> Testimonials </b-nav-item>
-          <b-nav-item> Articles </b-nav-item>
-          <b-nav-item>ToDo</b-nav-item>
-          <b-nav-item> Get In Touch </b-nav-item>
-        </b-navbar-nav>
-      </b-collapse>
-      <!-- <b-collapse id="nav-collapse" is-nav>
+    <nav class="navbar fixed-bottom navbar-light bg-light">
+      <a class="navbar-brand" href="#">Fixed bottom</a>
+      <div class="footer-container">
+        <div class="description-container">
+          <img src="" />
+          <div class="description">
+            I am a Front-end Developer with 12 years commercial experience,
+            author of CSS3 Foundations, and graduate of Internet Technology. I
+            create successful websites that are fast, easy to use, and built
+            with best practices.
+          </div>
+        </div>
+      </div>
+    </nav>
+  </div>
+  <!-- <b-collapse id="nav-collapse" is-nav>
         <b-navbar-nav>
           <b-nav-item href="#">Link</b-nav-item>
           <b-nav-item href="#" disabled>Disabled</b-nav-item>
         </b-navbar-nav> -->
 
-      <!-- Right aligned nav items -->
-      <!-- <b-navbar-nav class="ml-auto">
+  <!-- Right aligned nav items -->
+  <!-- <b-navbar-nav class="ml-auto">
         <b-nav-form>
           <b-form-input
             size="sm"
@@ -35,25 +35,24 @@
         </b-nav-form>
       </b-navbar-nav> -->
 
-      <!-- <b-nav-item-dropdown text="Lang" right>
+  <!-- <b-nav-item-dropdown text="Lang" right>
             <b-dropdown-item href="#">EN</b-dropdown-item>
             <b-dropdown-item href="#">ES</b-dropdown-item>
             <b-dropdown-item href="#">RU</b-dropdown-item>
             <b-dropdown-item href="#">FA</b-dropdown-item>
           </b-nav-item-dropdown> -->
 
-      <!-- <b-nav-item-dropdown right> -->
-      <!-- Using 'button-content' slot -->
-      <!-- <template v-slot:button-content>
+  <!-- <b-nav-item-dropdown right> -->
+  <!-- Using 'button-content' slot -->
+  <!-- <template v-slot:button-content>
               <em>User</em>
             </template>
             <b-dropdown-item href="#">Profile</b-dropdown-item>
             <b-dropdown-item href="#">Sign Out</b-dropdown-item>
           </b-nav-item-dropdown> -->
-      <!-- </b-navbar-nav>
+  <!-- </b-navbar-nav>
        </b-collapse> -->
-    </b-navbar>
-  </div>
+  <!-- </b-navbar> -->
 </template>
 
 <script>
